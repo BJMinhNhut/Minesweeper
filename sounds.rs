bae2e6b6d42bd188f741231fa4f91f48a4e56d6a
BJMinhNhut <nhutben678@gmail.com>
2023-01-02 15:20:03 +0700

    Add game sound

--- sounds.rs
+++
@@ -1,9 +0,0 @@
-#include "include/resource.h"
-
-CLICK WAVE "assets\\click.wav"
-MENU_BGM WAVE "assets\\menu_bgm.wav"
-EXPLODE WAVE "assets\\explosion.wav"
-VALID_BOX WAVE "assets\\valid.wav"
-WIN WAVE "assets\\win.wav"
-FLAG WAVE "assets\\flag.wav"
-NONE WAVE "assets\\none.wav"
