5f90249ff3c939837a1dacac32465d4214597a70
BJMinhNhut <nhutben678@gmail.com>
2023-01-03 11:58:49 +0700

    update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,9 @@
 @echo off
-mkdir resources
-windres sources/sounds.rs -O coff -o resources/sounds.res
-windres sources/icon.rc -O coff -o resources/icon.res
-g++ sources/main.cpp resources/sounds.res resources/icon.res -o Minesweeper -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 -static -static-libgcc -static-libstdc++ -O2 -lwinmm -mwindows
+windres sources/sounds.rs -O coff -o sounds.res
+windres sources/icon.rc -O coff -o icon.res
+g++ sources/main.cpp sounds.res icon.res -o Minesweeper -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 -static -static-libgcc -static-libstdc++ -O2 -lwinmm -mwindows
 if %ERRORLEVEL% NEQ 0 (
 	echo Compile error 
-) else echo Compile successfull
+) else echo Compile successful
+del sounds.res
+del icon.res
